370fb1475ff27e653ba96b13658503a2f06e84a3
Melany Yeung <mwyeung21@gmail.com>
2018-06-20 18:38:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+function getUKTime()
+{
+  return new Date().getHours() + 5 //midwest time
+}
+
+//getUKTime(5)
+
+function discountTicket(oriAmt)
+{
+  return oriAmt * 0.80
+}
+
+function tweetReply(str)
+{
+  return "Hey " + str + ", thanks so much for the support! I really appreciate it!";
+  
+  // why doesn this works?/cahnge color? return "Hey ${str} thanks!"
+  
+
+}
+
+
+
+
